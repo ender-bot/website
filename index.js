fd50c952f212eb9928931a5dd02dbc4cb1b4d253
Ender <xenderpugx@gmail.com>
2019-05-19 15:22:54 +0100

    🧱 Oauth

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const express = require("express");
 const log = require("./utilities/log");
 const path = require("path");
 const favicon = require('serve-favicon');
+const phin = require("phin");
+const client_secret = require("./utilities/secret.json").secret;
 
 const app = express()
 const port = 60001;
@@ -15,6 +17,24 @@ app.get('/', (req, res) => {
     res.render("home.pug", { oauth: { username: "Test", password: "Test", avatar: "https://cdn.discordapp.com/avatars/371685425351229441/82967dd71e6583754d573305464fc556.png?size=1024" }, favicon: 'i/favicon.ico' })
 });
 
+app.get("/login", async, (req, res) => {
+    var code = req.query.code;
+    var data = { 
+        client_id: '579661562688700417', 
+        client_secret, 
+        grant_type: 'authorization_code', 
+        code, 
+        redirect_uri: 'https://localhost:60001/login', 
+        scope: 'identify guilds' 
+    }
+    const resp = await p({
+        url: 'https://discordapp.com/api/oauth2/token',
+        method: 'POST',
+        data
+    });
+    res.redirect(`/login?resp=${resp.access_token}`)
+});
+    
 app.get('/i/:file', (req, res) => {
     res.sendFile(__dirname+'\\views\\i\\'+req.params.file, { }, function(err) {
         if(err) {
