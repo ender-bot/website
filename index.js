e7897cb82a762134901a60106c4ec10955486fb0
Ender <xenderpugx@gmail.com>
2019-05-19 20:31:08 +0100

    🌠 Logging in now works

--- index.js
+++ index.js
@@ -7,32 +7,72 @@ const express = require("express");
 const log = require("./utilities/log");
 const path = require("path");
 const favicon = require('serve-favicon');
-const phin = require("phin");
+const p = require("phin");
 const client_secret = require("./utilities/secret.json").secret;
+const {wrapAsync} = require('@rimiti/express-async');
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const port = 60001;
+app.use(cookieParser())
   
-app.get('/', (req, res) => {
-    res.render("home.pug", { oauth: { username: "Test", password: "Test", avatar: "https://cdn.discordapp.com/avatars/371685425351229441/82967dd71e6583754d573305464fc556.png?size=1024" }, favicon: 'i/favicon.ico' })
-});
+app.get('/', wrapAsync(async function serveMe(req, res) {
+    var cookie = req.cookies;
+    const resp = await p({
+        url: 'https://discordapp.com/api/users/@me',
+        parse: 'json',
+        headers: {
+            "Authorization": `Bearer ${cookie.auth}`
+        }
+    });
+    const render = {
+        oauth: {
+            username: "User",
+            discriminator: "0000",
+            password: "@",
+            avatar: "https://cdn.discordapp.com/embed/avatars/0.png",
+            id: "0"
+        },
+        favicon: 'i/favicon.ico'
+    };
+    render.oauth.username = resp.body.username
+    render.oauth.discriminator = resp.body.discriminator
+    render.oauth.id = resp.body.id
+    if(resp.body.avatar) {
+        render.oauth.avatar = `https://cdn.discordapp.com/avatars/${resp.body.id}/${resp.body.avatar}.png?size=1024`
+    }
+    res.render("home.pug", render)
+}));
 
-app.get("/login", async, (req, res) => {
+
+app.get("/login", wrapAsync(async function yeet(req, res) {
     var code = req.query.code;
     var data = { 
         client_id: '579661562688700417', 
         client_secret, 
         grant_type: 'authorization_code', 
         code, 
-        redirect_uri: 'https://localhost:60001/login', 
+        redirect_uri: 'http://localhost:60001/login', 
         scope: 'identify guilds' 
     }
     const resp = await p({
         url: 'https://discordapp.com/api/oauth2/token',
         method: 'POST',
-        data
+        form: data,
+        parse: 'json'
     });
-    res.redirect(`/login?resp=${resp.access_token}`)
+    if(resp.body.error) {
+        res.json(resp.body)
+    }
+    else {
+        res.send(resp.body)
+    }
+    console.log(resp.body)
+    
+}));
+
+app.get("/add", (req, res) => {
+    res.redirect("https://discordapp.com/oauth2/authorize?client_id=371685425351229441&scope=bot&permissions=536341759&response_type=code&redirect_uri=https%3A%2F%2Fbot.ender.site%2Fthanks")
 });
     
 app.get('/i/:file', (req, res) => {
